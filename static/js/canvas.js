4ffd37dbbc927bec47fa911ddab04c3c7ffbd394
croutons <myxie>
2017-01-26 14:10:50 +0800

    Added text box and instruction generator

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -97,40 +97,62 @@ $("canvas").click(function(e) {
         var coordinateString = gx + ',' + gy;
         coordinateMap.set(coordinateString, current_colour);
         drawBoard();
-        console.log(gx + ', ' + gy);
+        console.log(gx + ',' + gy);
     }
 });
 
+instructions_str = '';
+var map_string = 'map_string\n';
 $("#coordinates").click(function(e){
     for (var [key, value] of coordinateMap) {
         console.log(key + " = " + value);
-    }   
+        coord_string = String(key) + ' = ' + value +'\n';
+        map_string += coord_string;
+    }
+    console.log('Generating Instructions');
+    generateInstructions();
+    
+    function generateInstructions() {
+        x_val = 0;
+        y_val = 0;
+        console.log('Inside function');
+        for(var [key, value] of coordinateMap){
+            x_y_array = key.split(',');
+            x_next = parseInt(x_y_array[0]);
+            y_next = parseInt(x_y_array[1]);
+            console.log(x_next + ',' + y_next);
+            colour = value;
+
+            x_instruction = x_next - x_val;
+            y_instruction = y_next - y_val;
+
+            console.log(x_instruction + ',' + y_instruction);
+
+            if(x_instruction < 0){
+                x_operation_str = 'Subtract ' + Math.abs(x_instruction) + ' from x\n';
+            } else {
+                x_operation_str = 'Add ' + x_instruction + ' to x\n';
+            }
+            instructions_str +=x_operation_str;
+            if(y_instruction < 0){
+                y_operation_str = 'Subtract ' + Math.abs(y_instruction) + ' from y\n';
+            } else {
+                y_operation_str = 'Add ' + y_instruction + ' to y\n';
+            }
+            instructions_str +=y_operation_str;
+
+            instructions_str += 'Get colour ' + colour + '\n';
+            instructions_str += 'Plot(x,y,' + colour + ')\n';
+
+            x_val = x_next;
+            y_val = y_next;
+            console.log(x_val +',' + y_val);
+        }
+    }
+
+    $("#instructions").append(instructions_str);
+    $("#instructions").select();
 });
-/************************************************************/
 
-// (function () {
-// var textFile = null,
-//     makeTextFile = function (text) {
-//     var data = new Blob([text], {type: 'text/plain'});
+/****************Instruction logic************************/
 
-//     // If we are replacing a previously generated file we need to
-//     // manually revoke the object URL to avoid memory leaks.
-//     if (textFile !== null) {
-//         window.URL.revokeObjectURL(textFile);
-//     }
-
-//     textFile = window.URL.createObjectURL(data);
-
-//     return textFile;
-//     };
-
-
-//     var create = document.getElementById('create'),
-//     textbox = document.getElementById('textbox');
-
-//     create.addEventListener('click', function () {
-//         var link = document.getElementById('downloadlink');
-//         link.href = makeTextFile(textbox.value);
-//         link.style.display = 'block';
-//     }, false);
-// })();
